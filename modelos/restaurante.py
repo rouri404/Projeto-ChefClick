5c3792f5bea7b94cafc51d1dc2f2eb7b8dc7aba1
Gabriel Couto <gabriel.couto2704@gmail.com>
2025-04-16 21:46:09 -0300

    Ajustando restaurente.py e criando exercicios.ipynb

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,24 +2,29 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self._ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.title()
+        self._ativo = False     # O 'underline' serve como um privatizador de atributos
         Restaurante.restaurantes.append(self)
     
     def __str__(self):      
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(15)} | {'Status'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(15)} | {restaurante.ativo}')
+
     @property
     def ativo(self):
         return '☑' if self._ativo else '☐'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
-    def listar_restaurantes():
-        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(15)} | {'Status'}')
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(15)} | {restaurante.ativo}')
-
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+restaurante_praca = Restaurante('praça', 'gourmet')
+restaurante_praca.alternar_estado()
+restaurante_pizza = Restaurante('pizza express', 'italiana')
 
 Restaurante.listar_restaurantes()
