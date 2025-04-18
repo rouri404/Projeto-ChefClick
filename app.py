f22d9315ff5eaa03221031d118812777942627fa
Gabriel Couto <gabriel.couto2704@gmail.com>
2025-04-18 14:11:52 -0300

    Ajustando output do app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from modelos.restaurante import Restaurante
 
 restaurante_praca = Restaurante('praça', 'gourmet')
-restaurante_praca.receber_avaliacao('Gabriel', 10)
-restaurante_praca.receber_avaliacao('Felipe', 7)
-restaurante_praca.receber_avaliacao('Julia', 5)
+restaurante_praca.receber_avaliacao('Gabriel', 5)
+restaurante_praca.receber_avaliacao('Felipe', 3.5)
+restaurante_praca.receber_avaliacao('Julia', 2.5)
 
-# restaurante_mexicano = Restaurante('mexican food', 'mexicana')
-# restaurante_japones = Restaurante('japa', 'japonesa')
+restaurante_mexicano = Restaurante('mexican food', 'mexicana')
+restaurante_mexicano.receber_avaliacao('Maria', 3)
+restaurante_mexicano.receber_avaliacao('André', 4)
+restaurante_mexicano.receber_avaliacao('Camila', 4.5)
+restaurante_mexicano.alternar_estado()
 
-#restaurante_mexicano.alternar_estado()
+restaurante_japones = Restaurante('japa', 'japonesa')
 
 def main():
     Restaurante.listar_restaurantes()
